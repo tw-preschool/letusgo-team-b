99ccd347b878715a9ee84ba46df2fa2d2f1eba5c
zhen6939 <zhen6939@163.com>
2014-11-28 22:31:39 +0800

    hightlight current page at nav-bar

---
+++ public/scripts/highlightCurrentPage.js
@@ -0,0 +1,33 @@
+/**
+ * Created by liu on 11/28/14.
+ */
+$(document).ready(function(){
+    var highlightCurrentPage = function(){
+        // this will get the full URL at the address bar
+        var url = window.location.href;
+
+        // passes on every "a" tag
+        $(".container-fluid a").each(function() {
+            // checks if its the same on the address bar
+            if (url == (this.href)) {
+                $(this).closest("li").addClass("active");
+//                $(this).addClass("active");
+
+            }
+        });
+    }
+
+    highlightCurrentPage();
+//    $(function() {
+//        // this will get the full URL at the address bar
+//        var url = window.location.href;
+//
+//        // passes on every "a" tag
+//        $(".container-fluid a").each(function() {
+//            // checks if its the same on the address bar
+//            if (url == (this.href)) {
+//                $(this).closest("li").addClass("active");
+//            }
+//        });
+//    });
+});
