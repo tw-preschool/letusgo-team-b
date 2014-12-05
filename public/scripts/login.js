1ff77080af2bfbc418421a852f0f994c937688d5
tianmali <408267415@qq.com>
2014-12-05 22:58:15 +0800

    delete loginJs

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,14 +1,26 @@
-$("#login").on("click",function(){
-    var username = $("#inputUsername").val();
-    var password = $("#inputPassword").val();
-    loginCheck(username, password);
-    return false;
-});
-
-function loginCheck(username, password) {
-    if(username != "admin" || password != "admin") {
-        $(".message-danger > p").show();
-    } else {
-        location.href = "items.html";
+$(document).ready(function(){
+  $("#login").on('click',function(event){
+    event.preventDefault();
+    var name = $('#inputUsername').val();
+    var password = $('#inputPassword').val();
+    if(name.length === 0 || password.length === 0){
+      alert("用户名或密码不能为空");
+      return;
     }
-}
+    $.ajax({
+      type : "POST",
+      url : "/login",
+      data : {"name":name,"password":password},
+      dataType : "json",
+      success : function(result){
+        if(result === true){
+          window.location.href = '/admin';
+
+        }else{
+          $(".message-danger > p").show();
+        }
+      }
+
+    });
+  });
+});
