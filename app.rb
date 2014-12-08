c0aa9aba003f079ee0476a1570f779a6225964cd
young-chen <chennyoung@163.com>
2014-12-08 12:53:35 +0800

    [#005][ChenYang&TianMali][fix session]

--- app.rb
+++ app.rb
@@ -63,7 +63,6 @@ class POSApplication < Sinatra::Base
     end
 
     get '/' do
-    #  session[:isLogin] = false
       content_type :html
       erb :index
     end
@@ -98,7 +97,7 @@ class POSApplication < Sinatra::Base
     end
 
     get '/admin' do
-      if session[:isLogin] === false
+      if !(session[:isLogin] === true)
         redirect '/login'
       else
         content_type :html
