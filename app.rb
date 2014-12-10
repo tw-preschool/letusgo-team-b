131f2da717d2324b351acdc1d9ce0a689b03338b
tianmali <408267415@qq.com>
2014-12-10 20:51:40 +0800

    finished add to cart with '+'

--- app.rb
+++ app.rb
@@ -138,10 +138,6 @@ class POSApplication < Sinatra::Base
       erb :cart
     end
 
-    post '/cart' do
-      [201, {:message => "cart"}.to_json]
-    end
-
     get '/payment' do
       content_type :html
       erb :payment
--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,39 +1,42 @@
 $(document).ready(function(){
+  var appendCart = function(item,i){
+    var tr = $('<tr>\
+                  <td id=\"item-id\" hidden>' + (++i) + '</td>\
+                  <td>' + item.name + '</td>\
+                  <td>' + item.price + '</td>\
+                  <td>' + item.unit + '</td>\
+                  <td>\
+                    <button type=\"button\">-</button>\
+                    <input value='+cartHandle.getCount(item.name+"num")+'>\
+                    <button type=\"button\" class="add-cart">+</button>\
+                  </td>\
+                  <td class=\"font-color-red\" id=\"item-promotion\">买二送一</td>\
+                  <td id = \"item-num\">3</td>\
+                  </tr>');
+    $("#cart-table").append(tr);
+  };
+  var showCartItem = function(){
+    var storage = window.sessionStorage;
+        for(var i=0 ; i<storage.length;i++){
+            var key = storage.key(i);
+            if(key.indexOf("num") < 0){
+                console.log(key);
+                var item = cartHandle.getItem(key);
+                appendCart(item, i);
+            }
+        }
+  };
+
+  showCartItem();
   $(".add-cart").on('click',function(){
+    $("#cart-table").empty();
     var id = $(this).parent().siblings()[0].innerHTML;
-    console.log("id"+id);
       var name = $(this).parent().siblings()[1].innerHTML;
-      console.log("name"+name);
       var price = $(this).parent().siblings()[2].innerHTML;
       var unit = $(this).parent().siblings()[3].innerHTML;
       var pro = new product(name,price,unit);
       cartHandle.addItem(name,pro);
       pro.setProCount(cartHandle.getCount(name+"num"));
-
-      console.log(pro.getProCount());
-      $.ajax({
-        type: "POST",
-        url : "/cart",
-        data : {"name": name},
-        dataType : "json",
-        success : function(data){
-          if(pro.name != null){
-          var tr = $('<tr>\
-                        <td id=\"item-id\" hidden>' + id + '</td>\
-                        <td>' + pro.name + '</td>\
-                        <td>' + pro.price + '</td>\
-                        <td>' + pro.unit + '</td>\
-                        <td>\
-                          <button type=\"button\">-</button>\
-                          <input value='+pro.getProCount()+'>\
-                          <button type=\"button\" class=\"add-cart\">+</button>\
-                        </td>\
-                        <td class=\"font-color-red\" id=\"item-promotion\">买二送一</td>\
-                        <td id = \"item-num\">3</td>\
-                        </tr>');
-            $("#cart-table").append(tr);
-          }
-        }
-      });
+      window.location.href='/cart';
   });
 });
