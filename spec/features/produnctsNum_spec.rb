8e360b32481d02c2e7d632b2674a40bdfbd48acb
lyj <liyujing1990X@outlook.com>
2014-12-12 22:36:21 +0800

    test file for product management

--- spec/features/produnctsNum_spec.rb
+++ spec/features/produnctsNum_spec.rb
@@ -3,14 +3,49 @@
 require_relative '../spec_helper'
 
 describe 'Products Management' do
-  describe 'Manage Products Num ' do
-    before { get '/products' }
 
-    it 'is successful' do
+  describe 'Manager login ' do
+    it ' successfully' do
+      get '/login'
       expect(last_response.status).to eq 200
     end
-
-    
   end
+
+  describe 'add a product' do
+    let(:body) { {:name => "peach", :price => 12, :unit => "kg"} }
+
+    it 'add a product'  do
+      post '/products', body, {'Content-Type' => 'application/json'}
+      expect(last_response.status).to eq 201
+
+      get '/products'
+      created = JSON.parse(last_response.body)[0]
+
+      expect(created['name']).to eq "peach"
+      expect(created['price']).to eq 12
+      expect(created['unit']).to eq "kg"
+    end
+
+  end
+
+  describe 'delete a product ' do
+    it ' successfully' do
+      post '/products'
+      expect(last_response.status).to eq 201
+    end
+  end
+
+  describe 'fix product info ' do
+    it 'get the product' do
+      get '/products'
+      expect(last_response.status).to eq 200
+    end
+    it ' fix successfully' do
+      post '/item-edit'
+      expect(last_response.status).to eq 201
+    end
+  end
+
+
 
 end
