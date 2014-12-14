85738b8e3553fe6e62cd42894e3659ec1adec872
young-chen <chennyoung@gmail.com>
2014-12-14 20:32:10 +0800

    [#011][CY&TML][add create user table code]

---
+++ db/20141214195811_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :name
+      t.string :address
+      t.integer :phone
+      t.string :role
+      t.string :state
+      t.timestamps
+    end
+  end
+end
